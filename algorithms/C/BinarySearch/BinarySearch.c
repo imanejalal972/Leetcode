baf84e17b9be0f3b874b46a840922dfe50e78659
Imane Jalal <78828566+imanejalal972@users.noreply.github.com>
2022-03-26 18:25:08 +0100

    Add files via upload

---
+++ algorithms/C/BinarySearch/BinarySearch.c
@@ -0,0 +1,9 @@
+int search(int* nums, int numsSize, int target){
+    int start_index = 0, end_index = numsSize - 1;
+        
+        for (int i = 0; i <= end_index; i++) {
+            if (nums[i] == target) 
+                return i;
+         }
+        return -1;
+}
