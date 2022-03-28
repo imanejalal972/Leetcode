b0f9eeb60ef90bd6d31be46eea6e19842a0a9248
Imane Jalal <78828566+imanejalal972@users.noreply.github.com>
2022-03-28 15:52:48 +0200

    Add files via upload

---
+++ algorithms/C/SearchinRotatedSortedArrayII/SearchinRotatedSortedArrayII.c
@@ -0,0 +1,7 @@
+bool search(int* nums, int numsSize, int target){
+    for (int i = 0; i < numsSize; i++) {
+            if (nums[i] == target)
+                return true;
+        }
+        return false;
+}
